e6e0f08e7d86c3d3f8e2a699116584d6195f9d63
Yashpal Gawali <gawaliyashpal@gmail.com>
2025-09-12 17:49:05 +0530

    First Commit

---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+	private final ChatClient chatclient;
+
+	public ChatController(ChatClient.Builder chatClientBuilder) {
+		super();
+		this.chatclient = chatClientBuilder.build();
+	}
+
+	@GetMapping("/chat")
+	public String chat(@RequestParam String msg) {
+		return chatclient.prompt(msg).call().content();		 
+	}
+}
