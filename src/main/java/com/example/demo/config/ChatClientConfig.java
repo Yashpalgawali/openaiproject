46788fc40c9af17625298f89b2729addea8826c6
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-08-04 14:15:00 +0530

    defaultSystem() and defaultUser() is used in the application

---
+++ src/main/java/com/example/demo/config/ChatClientConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+	@Bean
+	ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+		return chatClientBuilder
+				.defaultSystem("""
+						Your are an internal HR Assistant. Your role is to help\s
+						employees with questions related to HR policies, such as \s
+						leave policies, working hours, benefits, and code of conduct.
+						If a user asks for help with anything outside of these topics.\s
+						kindly inform them that you can only assist with queries related to \s HR Policies. 
+						
+						""")
+				.defaultUser("How can you help me?")
+				.build();
+	}
+}
--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -12,25 +12,37 @@ public class ChatController {
 
 	private final ChatClient chatclient;
 
-	public ChatController(ChatClient.Builder chatClientBuilder) {
-		super();
-		this.chatclient = chatClientBuilder.build();
-	}
+//	public ChatController(ChatClient.Builder chatClientBuilder) {
+//		super();
+//		this.chatclient = chatClientBuilder
+//				.defaultSystem("""
+//						Your are an internal HR Assistant. Your role is to help\s
+//						employees with questions related to HR policies, such as \s
+//						leave policies, working hours, benefits, and code of conduct.
+//						If a user asks for help with anything outside of these topics.\s
+//						kindly inform them that you can only assist with queries related to \s HR Policies. 
+//						
+//						""")
+//				.defaultUser("How can you help me?")
+//				.build();
+//	}
 
+	public ChatController(ChatClient chatClient) {
+		this.chatclient = chatClient; 	}
+	
 	@GetMapping("/chat")
 	public String chat(@RequestParam String message) {
 		return chatclient
 //				.prompt(message)
 				.prompt()
 				.system("""
-						Your are an internal HR Assistant. Your role is to help\s
-						employees with questions related to HR policies, such as \s
-						leave policies, working hours, benefits, and code of conduct.
-						If a user asks for help with anything outside of these topics.\s
-						kindly inform them that you can only assist with queries related to \s HR Policies. 
+						Your are an internal IT helpdesk Assistant. Your role is to assist\s
+						employees with IT related issues, such as resetting passwords\s
+						unlocking accounts, and answering questions related to IT policies.
+						If a user requests help with anything outside of these responsibilities.\s
+						respond politely and inform them that you can only assist IT support tasks within your defined scope.
 						
 						""")
-
 				.user(message)
 				
 				.call().content();		 
